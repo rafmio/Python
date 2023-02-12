dd108929b7d93a33718e546a63a63e69dc65c853
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-02-12 16:14:07 +0300

    Home PC 12.02.2023 16:14

---
+++ src/overiq/dollar_at_the_end.py
@@ -0,0 +1,19 @@
+print("first line")
+print("second line")
+print("")
+
+print("first string", end="$")
+print("second string", end="$")
+print("third")
+print("")
+
+print("first", "second", "third")
+print("first", "second", "third", sep="#")
+print("")
+
+
+number = int(input("Enter a number: "))
+if number > 10:
+    print("Number is greater than 10")
+else:
+    print("Number is less than 10")
